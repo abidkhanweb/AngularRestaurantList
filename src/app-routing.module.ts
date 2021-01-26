432f5e06afa15e18000921b1591eac695e5accf9
abidkhanweb <abidkhanweb@gmail.com>
2021-01-26 17:32:43 +0530

    Add files via upload

---
+++ src/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { FavoriteProductComponent } from './favorite-product/favorite-product.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { HomeComponent } from './views/home/home.component';
+
+const routes: Routes = [
+  {path:'', component:HomeComponent, pathMatch:'full'},
+  {path:'home', component:HomeComponent},
+  {path:'product-detail/:id', component:ProductDetailComponent},
+  {path:'favorite-list', component:FavoriteProductComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
